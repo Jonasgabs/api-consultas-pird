8df955170a4336734dbd3670387e4382216d1bdf
Jonasgsf <jonas.sarmentof@gmail.com>
2025-03-20 22:10:44 -0300

    update: persistencia

--- app/cliente/cliente.py
+++ app/cliente/cliente.py
@@ -5,31 +5,36 @@ class Cliente:
     def __init__(self, host="127.0.0.1", porta=8080):
         self.host = host
         self.porta = porta
+        self.conexao = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            self.conexao.connect((self.host, self.porta))
+        except ConnectionError:
+            print("Erro: Não foi possível conectar ao servidor.")
+            self.conexao = None
 
     def enviar_requisicao(self, consulta):
+        if not self.conexao:
+            return "Sem conexão com o servidor."
+
         try:
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as cliente:
-                cliente.connect((self.host, self.porta))
-                requisicao = consulta.strip() + "\n"
-                sleep(0.5)
-                print(f"[DEBUG] Enviando: {requisicao.strip()}")
-                cliente.sendall(requisicao.encode())
+            requisicao = consulta.strip() + "\n"
+            sleep(0.2)
+            print(f"[DEBUG] Enviando: {requisicao.strip()}")
+            self.conexao.sendall(requisicao.encode())
 
-                resposta = cliente.recv(1024).decode()
-                sleep(0.5)
-                sleep(0.5)
-                print("-=" * 50)
-                sleep(0.5)
-                return resposta
+            resposta = self.conexao.recv(1024).decode()
+            sleep(0.2)
+            print("-=" * 50)
+            sleep(0.2)
+            return resposta
         except ConnectionError:
-            return "Erro: Não foi possível conectar ao servidor."
-
+            return "Erro ao enviar a requisição."
 
     def desconectar(self):
-        
-        try:
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as cliente:
-                cliente.connect((self.host, self.porta))
-                cliente.sendall("QUIT\n".encode())
-        except ConnectionError:
-            pass 
+        if self.conexao:
+            try:
+                self.conexao.sendall("QUIT\n".encode())
+                self.conexao.close()
+                self.conexao = None
+            except ConnectionError:
+                pass
